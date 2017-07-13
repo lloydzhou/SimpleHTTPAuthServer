0236f40db1be359a99de3fd57a8448fd15eeca0e
lloydzhou <lloydzhou@qq.com>
2017-07-13 20:17:31 +0800

    增加时间倒序排列

--- SimpleHTTPAuthServer/__main__.py
+++ SimpleHTTPAuthServer/__main__.py
@@ -3,12 +3,16 @@ A simple authenticated web server handler
 '''
 
 from SimpleHTTPServer import SimpleHTTPRequestHandler
+import cgi
+import urllib
+import time
 import os
 import sys
 import base64
 import ssl
 import SocketServer
 import argparse
+from StringIO import StringIO
 
 from . import __prog__
 
@@ -47,6 +51,43 @@ class SimpleHTTPAuthHandler(SimpleHTTPRequestHandler):
             self.do_authhead()
             self.wfile.write(self.headers.getheader('Authorization'))
             self.wfile.write('not authenticated')
+
+    def list_directory(self, path):
+        try:
+            flist = os.listdir(path)
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
+        flist.sort(key=lambda a: os.path.getmtime(os.path.join(path, a)), reverse=True)
+        f = StringIO()
+        displaypath = cgi.escape(urllib.unquote(self.path))
+        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
+        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
+        f.write("<hr>\n<ul>\n")
+        for name in flist:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            date_modified = time.ctime(os.path.getmtime(fullname))
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            f.write('<li><a href="%s">%s - %s</a>\n'
+                    % (urllib.quote(linkname), cgi.escape(displayname), date_modified))
+        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        encoding = sys.getfilesystemencoding()
+        self.send_header("Content-type", "text/html; charset=%s" % encoding)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return f
+
 
 def serve_https(https_port=80, https=True, start_dir=None, handler_class=SimpleHTTPAuthHandler):
     ''' setting up server '''
